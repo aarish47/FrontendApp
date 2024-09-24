0d05f1406abc6536b76b0e4b10553dbf7dfb2121
aarish47 <aarishk1429@gmail.com>
2024-09-24 18:44:30 +0500

    Inital Commit

--- src/features/tasks/taskService.js
+++ src/features/tasks/taskService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = '/api/tasks/'
+const API_URL = 'https://backendapp2-6vkjczwu.b4a.run/api/tasks/'
 
 const createTask = async (taskData, token) => {
     const config = {
